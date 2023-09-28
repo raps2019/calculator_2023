5218ecd27979a761878eb2fd0830ed5312f6bad4
Douglas Colaco <douglascolaco@gmail.com>
2023-09-27 23:16:13 -0600

    fixed large number displaying issues

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import { divide, multiply, subtract, add, opposite, operate } from './operators.
 
 let previousNumberDisplay = document.querySelector('#previousNumber')
 let currentNumberDisplay = document.querySelector('#currentNumber')
-let totalDisplay = document.querySelector('#total')
 
 
 
@@ -13,28 +12,51 @@ let operatorButtons = document.querySelectorAll('.operatorButton')
 let equalsButton = document.querySelector('.equalsButton')
 let mainDisplay = document.querySelector('.mainDisplay')
 let secondaryDisplay = document.querySelector('.secondaryDisplay')
+let mainDisplayText = ""
 let secondaryDisplayText = ""
 let currentNumber = ""
 let previousNumber = ""
 let total = ""
 let currentOperator = ""
+let currentOperatorSymbol = ""
 let secondaryDisplayCopy = ""
+let block = false
+let secondaryDisplayMaxLength = 34
+
 
 numberButtons.forEach(button => {
     button.addEventListener('click', e => {
-        if (currentOperator != "equals") {
-            // if (currentNumber.length > 17) {
-            //     secondaryDisplayCopy = mainDisplay.innerHTML
-            //     secondaryDisplay.innerHTML = "Number Limit Reached"
+        if (currentOperator != "equals" && block == false) {
+            // if (secondaryDisplay.innerHTML.length > 34) {
+            //     secondaryDisplayCopy = secondaryDisplay.innerHTML
+            //     secondaryDisplay.innerHTML = "Limit Reached"
+            //     block = true;
             //     setTimeout(() => {
-            //         secondaryDisplay.innerHTML = secondaryDisplayCopy;
+            //         secondaryDisplay.innerHTML = secondaryDisplayCopy
+            //         block = false
             //     }, 750)
             //     return;
             // }
             currentNumber = `${currentNumber}${e.target.id}`
-            console.log(currentNumber)
-            console.log(previousNumber)
-            secondaryDisplay.innerHTML = secondaryDisplay.innerHTML + e.target.id
+            secondaryDisplayText = secondaryDisplay.innerHTML + e.target.id
+            if (secondaryDisplayText.length > secondaryDisplayMaxLength) {
+                console.log("running")
+                secondaryDisplayText = `${previousNumber}${currentOperatorSymbol}${currentNumber}`
+                if (secondaryDisplayText.length < secondaryDisplayMaxLength) {
+                    secondaryDisplay.innerHTML = secondaryDisplayText
+                } else {
+                    secondaryDisplayCopy = secondaryDisplay.innerHTML
+                    secondaryDisplay.innerHTML = "Limit Reached"
+                    block = true;
+                    setTimeout(() => {
+                        secondaryDisplay.innerHTML = secondaryDisplayCopy
+                        block = false
+                    }, 750)
+                    return;
+                }
+            } else {
+                secondaryDisplay.innerHTML = secondaryDisplay.innerHTML + e.target.id
+            }
             if (previousNumber != "") {
                 switch (currentOperator) {
                     case "divide":
@@ -51,17 +73,32 @@ numberButtons.forEach(button => {
                         total = add(previousNumber, currentNumber)
                         break
                 }
+            }
+            if (total.toString().length > 15) {
+                console.log(total.length)
+                mainDisplay.innerHTML = total.toExponential(10)
+            } else {
                 mainDisplay.innerHTML = total
             }
         }
-        totalDisplay.innerHTML = total
-        previousNumberDisplay.innerHTML = previousNumber
-        currentNumberDisplay.innerHTML = currentNumber
+        // // totalDisplay.innerHTML = total
+        // previousNumberDisplay.innerHTML = previousNumber
+        // currentNumberDisplay.innerHTML = currentNumber
     });
 })
 
 operatorButtons.forEach(button => {
     button.addEventListener('click', e => {
+        // if (secondaryDisplay.innerHTML.length > secondaryDisplayMaxLength) {
+        //     secondaryDisplayCopy = secondaryDisplay.innerHTML
+        //     secondaryDisplay.innerHTML = "Limit Reached"
+        //     block = true
+        //     setTimeout(() => {
+        //         secondaryDisplay.innerHTML = secondaryDisplayCopy;
+        //         block = false
+        //     }, 750)
+        //     return;
+        // }
         if (currentNumber != '' || total != '') {
             if (total == '') {
                 previousNumber = currentNumber
@@ -71,37 +108,52 @@ operatorButtons.forEach(button => {
                 // currentNumber = ''
                 // mainDisplay.innerHTML = mainDisplay.innerHTML + e.target.innerHTML
             }
-            if (currentOperator == "equals") {
-                secondaryDisplay.innerHTML = `${previousNumber}${e.target.innerHTML}`
-            } else {
-                secondaryDisplay.innerHTML = `${secondaryDisplay.innerHTML}${e.target.innerHTML}`
-            }
-            // mainDisplay.innerHTML = mainDisplay.innerHTML + e.target.innerHTML
-            currentOperator = e.target.id
-            currentNumber = ''
         }
+        if (currentOperator == "equals") {
+            secondaryDisplay.innerHTML = `${previousNumber}${e.target.innerHTML}`
+        } else if (currentNumber == '') {
+            if (currentOperator != e.target.id) {
+                secondaryDisplay.innerHTML = secondaryDisplay.innerHTML.slice(0,-1)+e.target.innerHTML
+            }
+        }
+        else {
+            secondaryDisplay.innerHTML = `${secondaryDisplay.innerHTML}${e.target.innerHTML}`
+        }
+        // mainDisplay.innerHTML = mainDisplay.innerHTML + e.target.innerHTML
+        currentOperator = e.target.id
+        currentOperatorSymbol = e.target.innerHTML
+        currentNumber = ''
+
     })
 })
 
 equalsButton.addEventListener('click', e => {
-    if (currentOperator != "equals") {
+    if (currentOperator != "equals" && currentNumber != '') {
         currentOperator = e.target.id
+        currentOperatorSymbol = e.target.innerHTML
         secondaryDisplay.innerHTML = ''
         if (previousNumber == '') {
             previousNumber = currentNumber;
             currentNumber = ''
             total = previousNumber
-            mainDisplay.innerHTML = previousNumber
+            mainDisplayText = total
         } else {
-            mainDisplay.innerHTML = total
+            mainDisplayText = total
             previousNumber = total
             currentNumber = ''
+        }
+        if (total.toString().length > 15) {
+            console.log(total.length)
+            mainDisplay.innerHTML = total.toExponential(10)
+        } else {
+            mainDisplay.innerHTML = total
         }
     }
 })
 
 clearButton.addEventListener('click', e => {
     currentOperator = ''
+    currentOperatorSymbol = ''
     previousNumber = ''
     currentNumber = ''
     total = ''
@@ -112,7 +164,7 @@ clearButton.addEventListener('click', e => {
 deleteButton.addEventListener('click', e => {
     if (currentNumber !== "") {
         currentNumber = currentNumber.slice(0, -1)
-        mainDisplay.innerHTML = currentNumber
+        secondaryDisplay.innerHTML = secondaryDisplay.innerHTML.slice(0, -1)
     }
 })
 
